b6a255bd0e9a4e186794b67cddcae254e5b4cc1c
mjzilver <jari.zilverentant@gmail.com>
2019-03-13 23:47:45 +0100

    added a chat box

--- web.js
+++ web.js
@@ -4,6 +4,7 @@ var app = express();
 app.set('view engine', 'pug')
 
 var server = require('http').createServer(app)
+var io = require('socket.io').listen(server)
 var port = 1500
 
 const sqlite3 = require('sqlite3');
@@ -16,6 +17,10 @@ var Farm = require("./farm")
 
 // views folder is static so that it wont get to middleware
 app.use(express.static(__dirname + '/views'));
+
+app.use('/chat', function(req, res) {
+    res.render('chat');
+});
 
 app.use('/farms', function(req, res) {
     let selectSQL = 'SELECT * FROM farm';
@@ -32,6 +37,12 @@ app.use('/farms', function(req, res) {
 
 app.use('/', function(req, res) { 
     res.render('index') 
+});
+
+io.on('connection', function(socket){
+    socket.on('chat', function(msg){
+        io.emit('chat', msg);
+    });
 });
 
 // start server both
