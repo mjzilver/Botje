8b3ce0eb5b9d2fd2286e327c9c731883361513cf
mjzilver <jari.zilverentant@gmail.com>
2019-03-13 17:54:15 +0100

    seperated logger and added a web interface starter

---
+++ web.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug')
+
+var server = require('http').createServer(app)
+var port = process.env.PORT || 80
+
+// views folder is static so that it wont get to middleware
+app.use(express.static(__dirname + '/views'));
+
+app.use('/', function(req,res) { 
+
+    res.render('index') 
+});
+
+// start server both
+server.listen(port, () => {
+	console.log('App running on port ' + port)
+});
