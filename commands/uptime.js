8a689edc29562301d2fda04ed4c2282a98ab1bf7
mjzilver <mjzilver@avans.nl>
2020-11-11 19:36:37 +0100

    uptime

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,4 +1,11 @@
+var moment = require('moment');
 
 module.exports = function(message) {
-    
+    var login = moment(global.bot.readyTimestamp);
+	const now = moment();
+	hours = now.diff(login, 'hours');
+	minutes = now.subtract(hours, 'hours').diff(login, 'minutes');
+	seconds = now.subtract(minutes, 'minutes').diff(login, 'seconds');
+
+	message.channel.send(`I have been online for ${hours} hours, ${minutes} minutes and ${seconds} seconds.`)
 }
