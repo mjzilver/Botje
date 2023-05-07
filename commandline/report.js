09fbdb94851673c3d3830f1498997a9c4015170f
Mick Jari <jari.zilverentant@gmail.com>
2023-05-07 13:31:27 +0200

    added database_size to report

--- commandline/report.js
+++ commandline/report.js
@@ -1,4 +1,5 @@
 let logger = require("systems/logger.js")
+let database = require("systems/database.js")
 
 module.exports = {
     name: "report",
@@ -10,6 +11,11 @@ module.exports = {
             logger.console(`Memory: rss ${Math.round(rss / 1024 / 1024 * 100) / 100} MB`)
             logger.console(`Memory: heapUsed ${Math.round(heapUsed / 1024 / 1024 * 100) / 100} MB`)
             logger.console(`Memory: heapTotal ${Math.round(heapTotal / 1024 / 1024 * 100) / 100} MB`)
+
+            let sql = "SELECT pg_size_pretty(pg_database_size('botdb')) AS size"
+            database.query(sql, null, (rows) => {
+                logger.console(`Database size: ${rows[0]["size"]}`)
+            })
         } catch (error) {
             logger.console(`Error: ${error.message}`)
         }
